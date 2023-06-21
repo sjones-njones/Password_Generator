2620b939fe105d1da6b435c58154857c3a77a68d
stephanie jones <sjones.njones@gmail.com>
2023-06-21 01:34:46 -0500

    added comments to working code

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,14 @@
 // Assignment Code
+// stores variable
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
   passwordText.value = password;
-
-
 }
-
-
-// var redButton = document.querySelector("#generate");
-// document.querySelector('#generate').addEventListener('click', promptMe);
-
-
-
-
+// stores variables for all possible password characters 
 var optionsNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var optionsABCLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var optionsABCUpper = optionsABCLower.map(optionsABCLower => optionsABCLower.toUpperCase());
@@ -27,44 +16,34 @@ var optionsSpCh = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '
 var useChars = [];
 var pwLength;
 
-
 function generatePassword() {
-  useChars = [];
+  useChars= [];
   promptMe();
   console.log(useChars);
-  if (useChars == []) {
-
+  if (useChars==[])  {
+    
   }
-  var pwText = "";
-
-
-
-
-  for (var i = 0; i < pwLength; i++) {
-
-    var rng = Math.floor(Math.random() * useChars.length);
+  var pwText= "";
+  for (var i=0; i<pwLength; i++) {
+  
+  var rng =  Math.floor(Math.random() * useChars.length);
     pwText += useChars[rng];
   }
   return pwText;
 }
 
-
+// determines password length using user input
 function promptMe() {
   var characterPrompt = Number(window.prompt("How many characters would you like in your password? Enter a number between 8 and 128."));
   if (characterPrompt < 8 || characterPrompt > 128 || isNaN(characterPrompt)) {
     window.alert("Please choose a number between 8 and 128.");
-
-
-  } else {
+  } else { 
     pwLength = characterPrompt;
     promptMeLower();
   }
 }
 
-
-// document.addEventListener('click', promptMeLower);
-
-
+// asks user if lowercase characters will be included
 function promptMeLower() {
   var lowerPrompt = window.confirm("Would you like lower case letters to be included in your password?");
   if (lowerPrompt) {
@@ -73,10 +52,7 @@ function promptMeLower() {
   promptMeUpper();
 }
 
-
-// document.addEventListener('click', promptMeUpper);
-
-
+// asks user if uppercase characters will be included
 function promptMeUpper() {
   var upperPrompt = window.confirm("Would you like upper case letters to be included in your password?");
   if (upperPrompt) {
@@ -85,10 +61,7 @@ function promptMeUpper() {
   promptMeNum();
 }
 
-
-// document.addEventListener('click', promptMeNum);
-
-
+// asks user if numbers will be included
 function promptMeNum() {
   var numPrompt = window.confirm("Would you like numbers to be included in your password?");
   if (numPrompt) {
@@ -97,10 +70,7 @@ function promptMeNum() {
   promptMeChar();
 }
 
-
-// document.addEventListener('click', promptMeChar);
-
-
+// asks user if special characters will be included
 function promptMeChar() {
   var specialPrompt = window.confirm("Would you like special characters to be included in your password?");
   if (specialPrompt) {
@@ -108,11 +78,5 @@ function promptMeChar() {
   }
 }
 
-
-// var index = Math.floor(Math.random() * optionsABCLower.length);
-// var password = options[index];
-// window.alert(password);
-
-
-// Add event listener to generate button
+// click event calls function and writes password as promised
 generateBtn.addEventListener("click", writePassword);
